fbe27cf63e574bfaca754d1cb9507c170ebff81c
sniperkattenskol <te15.nils.lindfors@umea.nti.se>
2018-02-20 15:15:27 +0100

    See my other commit
    Javascript nava fixed not working

---
+++ JS/mainJS.js
@@ -0,0 +1,13 @@
+
+//Javascript navbar stick to top
+window.onscroll = function() {navBarStickyFunction()};
+var navIdname = getElementById("navHead");
+var sticked = navIdname.offsetTop;
+
+function navBarStickyFunction(){
+	if (window.pageYOffset >= sticked) {
+		navIdname.classList.add("naVStickyClass");
+	} else {
+		navIdname.classList.remove("naVStickyClass");
+	}
+}
